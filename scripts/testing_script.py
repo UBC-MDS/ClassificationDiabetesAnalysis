16fd8c661acc9a0193f06187513300e65ea6ac48
Inderjit Khera <inder.khera@hotmail.com>
2024-12-06 16:54:50 -0800

    revised code for pull review

--- scripts/testing_script.py
+++ scripts/testing_script.py
@@ -19,23 +19,27 @@ from sklearn.metrics import fbeta_score, make_scorer
 @click.command()
 @click.option('--x-train-data', type=str, help="Path to X_train data")
 @click.option('--x-test-data', type=str, help="Path to X_train data")
+@click.option('--y-test-data', type=str, help="Path to X_train data")
 @click.option('--pipeline-from', type=str, help="Path to directory where the fit pipeline object lives")
-def main(x_train_data, x_test_data, pipeline_from):
+@click.option('--results-to', type=str, help="Path to directory where the table will be written to")
+@click.option('--plot-to', type=str, help="Path to directory where the plot will be written to")
+def main(x_train_data, x_test_data, y_test_data, pipeline_from, results_to, plot_to):
 
-    # read in random_fit model (pipeline object)
     
+    #read in csv files for training and testing model
     X_train = pd.read_csv(x_train_data)
     X_test = pd.read_csv(x_test_data)
-    y_test= pd.read_csv('./data/processed/y_test.csv')
+    y_test= pd.read_csv(y_test_data)
 
-
+    # read in random_fit model (pipeline object)
     with open(pipeline_from, 'rb') as f:
         random_fit = pickle.load(f)
 
-    pd.DataFrame(random_fit.cv_results_).sort_values(
-    "rank_test_score").head(3)[["mean_test_score",
+    mean_scores = pd.DataFrame(random_fit.cv_results_).sort_values(
+        "rank_test_score").head(3)[["mean_test_score",
                                 "mean_train_score"]]
     
+    mean_scores.to_csv(os.path.join(results_to, "mean_scores.csv"), index=False)
 
     # Best model from the search object
     best_model = random_fit.best_estimator_
@@ -53,11 +57,14 @@ def main(x_train_data, x_test_data, pipeline_from):
     # Sort by 'Coefficients' in descending order to see the most important features first
     coeff_df_sorted = coeff_df.sort_values(by = 'Coefficients', ascending = False)
 
-    coeff_df_sorted.style.format(
+    coeff_table = coeff_df_sorted.style.format(
         precision = 3
-    ).background_gradient(
-    axis = None
-    )
+        ).background_gradient(
+            axis = None
+            )
+    # Save the sorted coefficients to a CSV file
+    coeff_df_sorted.to_csv(os.path.join(results_to, "coeff_table.csv"), index=False)
+    coeff_table.to_html(os.path.join(results_to, 'coeff_table.html'))
 
     # Make predictions using the best model
     y_pred = best_model.predict(X_test)
@@ -71,34 +78,41 @@ def main(x_train_data, x_test_data, pipeline_from):
     pred_results_1_df['pred_bool'] = pred_results_1_df['pred_bool'] == 1
     pred_results_1_df.head()
 
+    pred_results_1_df.to_csv(os.path.join(results_to, "pred_results_1_df.csv"), index=False)
+
     # Compute accuracy
     accuracy = best_model.score(X_test, y_test)
 
-    pd.DataFrame({'accuracy': [accuracy]})
+    accuracy_df = pd.DataFrame({'accuracy': [accuracy]})
+    accuracy_df.to_csv(os.path.join(results_to, "accuracy_df.csv"), index=False)
 
     # Calculate the number of correct predictions and misclassifications
     value_counts = pred_results_1_df['pred_bool'].value_counts()
 
-    pd.DataFrame({
+    value_counts_df = pd.DataFrame({
         'correct predictions': [value_counts.get(True, 0)], 
         'misclassifications': [value_counts.get(False, 0)]
-    })
+        })
+    value_counts_df.to_csv(os.path.join(results_to, "value_counts_df.csv"), index=False)
 
     # Calculate the number of false positives (FPs) and false negatives (FNs)
     fp = len(pred_results_1_df[(pred_results_1_df['y_test'] == 0) & (pred_results_1_df['y_pred'] == 1)])
     fn = len(pred_results_1_df[(pred_results_1_df['y_test'] == 1) & (pred_results_1_df['y_pred'] == 0)])
 
-    pd.DataFrame({
+    fp_fn_df = pd.DataFrame({
         'false positives': [fp], 
         'false negatives': [fn]
-    })
-
-    alt.Chart(pred_results_1_df, title = 'Test Set Prediction Accuracy').mark_tick().encode(
-    x = alt.X('y_pred_prob_1').title('Positive Class Prediction Prob'),
-    y = alt.Y('pred_bool').title('Pred. Accuracy'),
-    color = alt.Color('y_test:N').title('Outcome')
-    )
+        })
     
+    fp_fn_df.to_csv(os.path.join(results_to, "fp_fn_df.csv"), index=False)
+
+    predic_chart = alt.Chart(pred_results_1_df, title = 'Test Set Prediction Accuracy').mark_tick().encode(
+        x = alt.X('y_pred_prob_1').title('Positive Class Prediction Prob'),
+        y = alt.Y('pred_bool').title('Pred. Accuracy'),
+        color = alt.Color('y_test:N').title('Outcome')
+        )
+    predic_chart.save(os.path.join(plot_to, 'predic_chart.png'),
+                            scale_factor=2.0)
 
 if __name__ == '__main__':
     main()
