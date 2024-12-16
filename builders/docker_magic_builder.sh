8b24aa91a8736e30125f31578ad36b9b80a6f53e
Jenny Zhang <yucizhang2008@gmail.com>
2024-12-16 02:37:29 -0800

    README update & builders update to clean up docker images before compose up

--- builders/docker_magic_builder.sh
+++ builders/docker_magic_builder.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Step 1: Build the Docker image and create the Conda environment
+# Step 1: clean up all containers and networks associated with `docker-compose.yml` project
+docker-compose down --rmi all --volumes
+
+# Step 2: Build the Docker image and create the Conda environment
 echo "Building the Docker image and setting up the Conda environment..."
 docker-compose up --build
 
-# Step 2: Confirm that the services are running
+# Step 3: Confirm that the services are running
 echo "Docker containers are up and running."
